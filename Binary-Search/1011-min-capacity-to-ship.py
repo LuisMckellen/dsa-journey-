62fe3a1746dedf0b097d0174df49ece22d99b2a9
Penguin <luismckellen777@gmail.com>
2026-08-29 11:47:45 +0530

    Implement binary search for shipping capacity

---
+++ Binary-Search/1011-min-capacity-to-ship.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def shipWithinDays(self, weights, days):
+        l=max(weights)
+        h=sum(weights)
+        def check(mid):
+            current_weight=0
+            daysn=1
+            for w in weights:
+                if current_weight+w>mid:
+                    daysn+=1
+                    current_weight=w
+                else:
+                    current_weight+=w
+            return daysn<=days
+            
+        while l<h:
+            mid=(l+h)//2
+            if check(mid):
+                h=mid
+            else:
+                l=mid+1
+        return l
